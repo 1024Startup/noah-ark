9fd8b8fac63b34c45d3b939c9fa31918fc1ce820
Kenneth <kenneth@influx.io>
2018-10-24 19:36:27 +0800

    add docker

---
+++ server/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+path=/Users/kenneth/workspace/noah-ark/server
+
+case $1 in
+    run)
+        echo $1
+        cd /go/src/noah
+        echo "building..."
+        go build
+        echo "build done"
+        ./noah
+        ;;
+    docker|*)
+        echo $1
+        docker run -it --rm -v ${path}:/go -p 1024:8080 golang:1.10 /bin/bash -c "/go/run.sh run"
+        ;;
+esac
--- server/src/noah/routers/router.go
+++ server/src/noah/routers/router.go
@@ -2,10 +2,14 @@ package routers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 	"noah/controllers"
 )
 
 func init() {
+	beego.Get("/", func(context *context.Context) {
+		context.Redirect(302, "/static/")
+	})
 	beego.Router("/api/apps/list", &controllers.AppController{}, "get:GetList")
 	beego.Router("/api/categories", &controllers.CategoryController{})
 	beego.Router("/api/categories/summary", &controllers.CategoryController{}, "get:GetSummary")
